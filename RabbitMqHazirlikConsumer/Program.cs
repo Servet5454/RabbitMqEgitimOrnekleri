961ebc1a5f2ab29a354b0d60ec8ea64300b2005b
Servet5454 <halicarnassus33@gmail.com>
2023-09-11 20:43:37 +0300

    danaout exchange davranışı yapıldı

--- RabbitMqHazirlikConsumer/Program.cs
+++ RabbitMqHazirlikConsumer/Program.cs
@@ -4,32 +4,60 @@ using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System.Text;
 
-internal class Program
+internal class Program //Consumer
 {
     private static void Main(string[] args)
     {
+        #region 1. RabbitMq Ön Hazırlık
+        //var factory = new ConnectionFactory();
+        //factory.Uri = new Uri("amqps://kzylzeod:w54vPXXqZXhIMKETehruwfRirfxpOZc-@moose.rmq.cloudamqp.com/kzylzeod");
+
+        //using var connection = factory.CreateConnection();
+        //var channel = connection.CreateModel();
+        //// channel.QueueDeclare("HelloQueve", true, false, false);
+
+
+        //channel.BasicQos(0, 1, false); //burada consumer lara  1 er 1 er dağıttık...
+        //var consumer = new EventingBasicConsumer(channel);
+        //channel.BasicConsume("HelloQueve", false, consumer);
+        //consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
+        //{
+        //    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+        //    Thread.Sleep(1000);
+        //    Console.WriteLine(message);
+        //    channel.BasicAck(e.DeliveryTag, false);
+        //};
+
+        //Console.ReadLine();
+        #endregion
+
+        #region 2. Fanaout Exchange Davranışı
+
         var factory = new ConnectionFactory();
         factory.Uri = new Uri("amqps://kzylzeod:w54vPXXqZXhIMKETehruwfRirfxpOZc-@moose.rmq.cloudamqp.com/kzylzeod");
 
         using var connection = factory.CreateConnection();
         var channel = connection.CreateModel();
-        // channel.QueueDeclare("HelloQueve", true, false, false);
+        //var randomquevename = channel.QueueDeclare().QueueName;
+        var randomquevename = "channel.QueueDeclare().QueueName";
+        channel.QueueDeclare(randomquevename, true, false, false);
+        channel.QueueBind(randomquevename, "Log-FanaoutExchange", "", null);
 
-
-        channel.BasicQos(0, 1, false); //burada consumer lara  1 er 1 er dağıttık...
+        channel.BasicQos(0, 1, false);
         var consumer =new EventingBasicConsumer(channel);
-        channel.BasicConsume("HelloQueve", false, consumer);
-        consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
+        channel.BasicConsume(randomquevename,false, consumer);
+        Console.WriteLine("Loglanıyor");
+        consumer.Received += (object sender, BasicDeliverEventArgs e) =>
         {
-            var message =Encoding.UTF8.GetString(e.Body.ToArray());
+            var message = Encoding.UTF8.GetString(e.Body.ToArray());
             Thread.Sleep(1000);
             Console.WriteLine(message);
             channel.BasicAck(e.DeliveryTag, false);
         };
-
+        #endregion
         Console.ReadLine();
 
     }
 
-  
+
 }
--- rabbitmqhazirlik/Program.cs
+++ rabbitmqhazirlik/Program.cs
@@ -3,28 +3,53 @@
 using RabbitMQ.Client;
 using System.Text;
 
-internal class Program
-{
+internal class Program //Publisher
+{   
     private static void Main(string[] args)
     {
-        var factory = new ConnectionFactory();
-        factory.Uri = new Uri("amqps://kzylzeod:w54vPXXqZXhIMKETehruwfRirfxpOZc-@moose.rmq.cloudamqp.com/kzylzeod");
+        #region 1. RabbitMq Ön Hazırlık Consumer
 
+        //var factory = new ConnectionFactory();
+        //factory.Uri = new Uri("amqps://kzylzeod:w54vPXXqZXhIMKETehruwfRirfxpOZc-@moose.rmq.cloudamqp.com/kzylzeod");
+
+        //using var connection = factory.CreateConnection();
+        //var channel = connection.CreateModel();
+        //channel.QueueDeclare("HelloQueve", true, false, false);
+
+        //Enumerable.Range(1, 50).ToList().ForEach(x =>
+        //{
+
+        //    string message = $"Selamın Aleyküm :{x}";
+        //    var messageBody = Encoding.UTF8.GetBytes(message);
+        //    channel.BasicPublish(string.Empty, "HelloQueve", null, messageBody);
+        //    Console.WriteLine(message);
+
+        //});
+
+        //Console.ReadLine();
+        #endregion
+
+        #region 2. Fanaout Exchange Davranışı
+        // burada senoryada biz exchange oluşturuyoruz kuyruk oluşturmuyoruz, böylelikle consumne edecek taraf kendi kuyruğunu oluşturup buradan direk bilgi alabilecekler....
+
+        var factory =new ConnectionFactory();
+        factory.Uri = new Uri("amqps://kzylzeod:w54vPXXqZXhIMKETehruwfRirfxpOZc-@moose.rmq.cloudamqp.com/kzylzeod");
         using var connection = factory.CreateConnection();
-        var channel = connection.CreateModel();
-        channel.QueueDeclare("HelloQueve", true, false, false);
+        var channel =connection.CreateModel();
+        channel.ExchangeDeclare("Log-FanaoutExchange", durable: true, type: ExchangeType.Fanout); //durable true diyoruz çünkü uygulama kapatılırsa exchange kaybolmasın istiyorum...
+
 
         Enumerable.Range(1, 50).ToList().ForEach(x =>
         {
-            
-            string message = $"Selamın Aleyküm :{x}";
+
+            string message = $"Fanaut Exchange mesajları kuyruk ayırt etmeksizin kaçtane kuyruk varsa hepsine aynı mesajı iletir :{x}";
             var messageBody = Encoding.UTF8.GetBytes(message);
-            channel.BasicPublish(string.Empty, "HelloQueve", null, messageBody);
+            channel.BasicPublish("Log-FanaoutExchange","", null, messageBody); //burada kuyruk declare etmiyoruz o yüzden çift tırnak ile boş bırakıyoruz çünkü fanaout kendisine bağlanan her kuyruğa ayırt etmeksizin aynı mesajı atacak...
             Console.WriteLine(message);
-          
+
         });
-       
-        Console.ReadLine();
+        #endregion
+
 
     }
 }
